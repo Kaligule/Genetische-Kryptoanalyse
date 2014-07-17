4d0b893f2a4e0c163ec8de8b29c14dedfb3922eb
Jøhannes Lippmann <code@schauderbasis.de>
2014-07-17 21:56:58 +0200

    get rid of executable GenSort

--- doStatistics.sh
+++ doStatistics.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 anzahlDurchlaufe="$1"
 
-rm GenSort
 ghc --make -outputdir=ghc_outputdir GenSort.hs
 echo "compiled"
 seq $anzahlDurchlaufe | parallel --gnu ./runOnceAndPlot.sh
+rm GenSort
