fbf8de8d9e2a8a3f9c712e4cebfdd2c5b3dd909d
Jøhannes Lippmann <code@schauderbasis.de>
2014-07-23 15:06:54 +0200

    replaced Genome a by [a] since it is not needed here.

--- SnModule.hs
+++ SnModule.hs
@@ -1,15 +1,13 @@
 module SnModule (makePeriodicS_n) where
-import Moo.GeneticAlgorithm.Binary (Genome)
-
 
 import Data.List (sortBy)
 import Data.Ord (comparing)
 import TypeModule
 
-makePeriodicS_n :: (Ord a) => Genome a -> S_n b
+makePeriodicS_n :: (Ord a) => [a] -> S_n b
 makePeriodicS_n genome = (periodicS_n (length genome) . ordGenomeToS_n) genome
 	where
-		ordGenomeToS_n :: (Ord a) => Genome a -> S_n b
+		ordGenomeToS_n :: (Ord a) => [a] -> S_n b
 		ordGenomeToS_n as = map snd . sortBy (comparing fst) . zip as
 		
 		periodicS_n :: Int -> S_n a -> S_n a
